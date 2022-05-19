27c97713c7aad03ae4229a38ad5af5e83f4ab35b
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-05-19 20:19:09 +0300

    Added Socket Server

---
+++ SocketServer/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {    
+    cors: {origin: "*"} //TODO: REMOVE THIS AT FINAL BUILD
+});
+
+app.use(express.static('public'));
+
+
+
+io.on("connection", (socket) => {
+    console.log(socket + " connected");
+});
+
+io.on('message',(socket) =>
+    
+)
+
+httpServer.listen(3000);
