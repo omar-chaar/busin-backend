589dff2577c4d6da80a306ac152d7f2ee0542e0e
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-05-21 21:33:07 +0300

    Added user login

--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,14 @@ const mysql = require('../lib/mysql').pool
 
 //functions
 
-function generateCode(req, res){
+function generateCode(req, res) {
     const departmentId = req.body.departmentId;
     const position = req.body.position;
     const name = req.body.name;
     const surname = req.body.surname;
     const admin = req.body.admin || false;
 
-    if(!departmentId || !position || !name || !surname){
+    if (!departmentId || !position || !name || !surname) {
         return res.status(400).send({
             error: 'Missing information.'
         });
@@ -21,12 +21,12 @@ function generateCode(req, res){
 
     let length = 5, chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", code = "";
     for (var i = 0, n = chars.length; i < length; ++i) {
-      code += chars.charAt(Math.floor(Math.random() * n
-      ));
+        code += chars.charAt(Math.floor(Math.random() * n
+        ));
     }
 
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -36,23 +36,23 @@ function generateCode(req, res){
             [name, surname, departmentId, position, admin, false, code],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                return res.status(200).send({response: 'Code generated. Send it to the user so he can finish the registration.', data: code});
+                return res.status(200).send({ response: 'Code generated. Send it to the user so he can finish the registration.', data: code });
             }
         );
     });
 }
 
-function validateCode(req, res){
+function validateCode(req, res) {
     const code = req.query.token;
-    
+
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -62,44 +62,44 @@ function validateCode(req, res){
             [code],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                if(results.length === 0){
+                if (results.length === 0) {
                     return res.status(400).send({
                         error: 'Invalid code.'
                     });
                 }
 
-                return res.status(200).send({response: 'Code is valid.'});
+                return res.status(200).send({ response: 'Code is valid.' });
             }
         );
     });
 }
 
-function createUserFromCode(req, res){
+function createUserFromCode(req, res) {
     const password = req.body.password;
     const email = req.body.email;
     const code = req.body.code;
 
-    if(!password || !email){
+    if (!password || !email) {
         return res.status(400).send({
             error: 'Missing password or email.'
         });
     }
 
     bcrypt.hash(password, 10, (err, hash) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
         }
 
         mysql.getConnection((err, connection) => {
-            if(err){
+            if (err) {
                 return res.status(500).send({
                     error: err
                 });
@@ -109,29 +109,29 @@ function createUserFromCode(req, res){
                 [hash, email, code],
                 (err, results) => {
                     connection.release();
-                    if(err){
+                    if (err) {
                         console.log(err)
                         return res.status(500).send({
                             error: 'Failed to register user. Try Again.'
                         });
                     }
 
-                    return res.status(200).send({response: 'User created.'});
+                    return res.status(200).send({ response: 'User created.' });
                 }
             )
         });
     });
 }
 
-function getUserData(req, res){
+function getUserData(req, res) {
     const userId = req.params.userId;
-    if(!userId){
+    if (!userId) {
         return res.status(400).send({
             error: 'Missing user id.'
         });
     }
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -141,41 +141,41 @@ function getUserData(req, res){
             [userId],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                if(results.length === 0){
+                if (results.length === 0) {
                     return res.status(400).send({
                         error: 'User not found.'
                     });
                 }
 
-                return res.status(200).send({response: 'User found.', data: results[0]});
+                return res.status(200).send({ response: 'User found.', data: results[0] });
             }
         );
     });
 }
 //routes
 
-function editUserData(req, res){
+function editUserData(req, res) {
     const userId = req.params.userId;
     const name = req.body.name;
     const surname = req.body.surname;
     const departmentId = req.body.departmentId;
     const position = req.body.position;
     const admin = req.body.admin || false;
-    
-    if(!userId || !name || !surname || !departmentId || !position){
+
+    if (!userId || !name || !surname || !departmentId || !position) {
         return res.status(400).send({
             error: 'Missing information.'
         });
     }
 
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -185,30 +185,30 @@ function editUserData(req, res){
             [name, surname, departmentId, position, admin, userId],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                return res.status(200).send({response: 'User updated.'});
+                return res.status(200).send({ response: 'User updated.' });
             }
         );
     });
 }
 
-function editUserEmail(req, res){
+function editUserEmail(req, res) {
     const userId = req.params.userId;
     const email = req.body.email;
 
-    if(!userId || !email){
+    if (!userId || !email) {
         return res.status(400).send({
             error: 'Missing information.'
         });
     }
 
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -218,38 +218,38 @@ function editUserEmail(req, res){
             [email, userId],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                return res.status(200).send({response: 'User updated.'});
+                return res.status(200).send({ response: 'User updated.' });
             }
         );
     });
 }
 
 //Change use password in safe manner
-function editUserPassword(req, res){
+function editUserPassword(req, res) {
     const userId = req.params.userId;
     const password = req.body.password;
 
-    if(!userId || !password){
+    if (!userId || !password) {
         return res.status(400).send({
             error: 'Missing information.'
         });
     }
 
     bcrypt.hash(password, 10, (err, hash) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
         }
 
         mysql.getConnection((err, connection) => {
-            if(err){
+            if (err) {
                 return res.status(500).send({
                     error: err
                 });
@@ -259,13 +259,13 @@ function editUserPassword(req, res){
                 [hash, userId],
                 (err, results) => {
                     connection.release();
-                    if(err){
+                    if (err) {
                         return res.status(500).send({
                             error: err
                         });
                     }
 
-                    return res.status(200).send({response: 'User updated.'});
+                    return res.status(200).send({ response: 'User updated.' });
                 }
             );
         });
@@ -273,18 +273,18 @@ function editUserPassword(req, res){
 
 }
 
-function updateUserImage(req, res){
+function updateUserImage(req, res) {
     const userId = req.params.userId;
     const image = req.body.image;
 
-    if(!userId || !image){
+    if (!userId || !image) {
         return res.status(400).send({
             error: 'Missing information.'
         });
     }
 
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -294,28 +294,28 @@ function updateUserImage(req, res){
             [image, userId],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                return res.status(200).send({response: 'User updated.'});
+                return res.status(200).send({ response: 'User updated.' });
             }
         );
     });
 }
 
 
-function getUserImage(req, res){
+function getUserImage(req, res) {
     const userId = req.params.userId;
-    if(!userId){
+    if (!userId) {
         return res.status(400).send({
             error: 'Missing user id.'
         });
     }
     mysql.getConnection((err, connection) => {
-        if(err){
+        if (err) {
             return res.status(500).send({
                 error: err
             });
@@ -325,23 +325,91 @@ function getUserImage(req, res){
             [userId],
             (err, results) => {
                 connection.release();
-                if(err){
+                if (err) {
                     return res.status(500).send({
                         error: err
                     });
                 }
 
-                if(results.length === 0){
+                if (results.length === 0) {
                     return res.status(400).send({
                         error: 'User not found.'
                     });
                 }
 
-                return res.status(200).send({response: 'User found.', data: results[0]});
+                return res.status(200).send({ response: 'User found.', data: results[0] });
             }
         );
     });
-} 
+}
+
+function userLogin(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(400).send({
+            error: 'Missing information.'
+        });
+    }
+
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).send({
+                error: err
+            });
+        }
+
+        mysql.getConnection((err, connection) => {
+            if (err) {
+                return res.status(500).send({
+                    error: err
+                });
+            }
+            connection.query(
+                'SELECT * FROM User WHERE email = ?',
+                [email],
+                (err, results) => {
+                    connection.release();
+                    if (err) {
+                        return res.status(500).send({
+                            error: err
+                        });
+                    }
+
+                    if (results.length === 0) {
+                        return res.status(400).send({
+                            error: 'Email incorrect.'
+                        });
+                    }
+
+                    bcrypt.compare(password, results[0].password, (err, result) => {
+                        if (err) {
+                            return res.status(500).send({
+                                error: err
+                            });
+                        }
+
+                        if (result) {
+                            jwt.sign({ userId: results[0] }, process.env.JWT_KEY, (err, token) => {
+                                if (err) {
+                                    return res.status(500).send({
+                                        error: err
+                                    });
+                                }
+
+                                return res.status(200).send({ response: 'User found.', data: results[0], token: token });
+                            });
+
+                        } return res.status(400).send({
+                            error: 'Password incorrect.'
+                        });
+                    });
+                }
+            );
+        });
+    });
+}
 
 router.post('/generate-code', generateCode); //this route will require admin authorization
 router.get('/validate-code', validateCode);
@@ -352,5 +420,6 @@ router.put('/edit-user-email/:userId', editUserEmail);
 router.put('/edit-user-password/:userId', editUserPassword);
 router.put('/update-user-image/:userId', updateUserImage);
 router.get('/get-user-image/:userId', getUserImage);
+router.post('/login', userLogin);
 
 module.exports = router;
