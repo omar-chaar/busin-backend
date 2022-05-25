f11b0b3650b0105b0222566de409a483b6f6d06d
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-05-25 22:21:36 +0300

    Added get 10  announcements

--- routes/announcement.js
+++ routes/announcement.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const { connect } = require('./user');
-const router = express.Router()
-const mysql = require('../lib/mysql').pool
-const adminAuth = require('../middlewares/adminAuthorization');
-const userAuth = require('../middlewares/userAuthorization');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { connect } = require("./user");
+const router = express.Router();
+const mysql = require("../lib/mysql").pool;
+const adminAuth = require("../middlewares/adminAuthorization");
+const userAuth = require("../middlewares/userAuthorization");
 
 //Creates an announcement to send to the whole company
 function createAnnouncement(req, res, next) {
@@ -15,95 +15,165 @@ function createAnnouncement(req, res, next) {
     mysql.getConnection((err, connection) => {
         if (err) {
             return res.status(500).send({
-                error: err
+                error: err,
             });
         }
         connection.query(
-            'INSERT INTO Announcement (announcement_title, announcement_body, sender_id) VALUES (?, ?, ?);',
+            "INSERT INTO Announcement (announcement_title, announcement_body, sender_id) VALUES (?, ?, ?);",
             [title, body, senderId],
             (err, results) => {
                 connection.release();
                 if (err) {
                     return res.status(500).send({
-                        error: err
+                        error: err,
                     });
                 }
                 res.locals.announcementId = results.insertId;
                 connection.query(
-                    'SELECT Company.company_id FROM Company INNER JOIN Department ON Department.company_id = Company.company_id INNER JOIN User ON Department.department_id = User.department_id WHERE User.user_id = ?;',
+                    "SELECT Company.company_id FROM Company INNER JOIN Department ON Department.company_id = Company.company_id INNER JOIN User ON Department.department_id = User.department_id WHERE User.user_id = ?;",
                     [senderId],
                     (err, results) => {
                         connection.release();
                         if (err) {
                             return res.status(500).send({
-                                error: err
+                                error: err,
                             });
                         }
                         res.locals.companyId = results[0].company_id;
                         next();
                     }
-                )//end of inner query
+                ); //end of inner query
             }
-        );//end of query
-    });//end of mysql.getConnection
-}//end of createAnnouncement
+        ); //end of query
+    }); //end of mysql.getConnection
+} //end of createAnnouncement
 
+//function necessary to create a new announcement
 function addAnnouncementReceivers(req, res) {
     const announcementId = res.locals.announcementId;
     const companyId = res.locals.companyId;
     mysql.getConnection((err, connection) => {
         if (err) {
             return res.status(500).send({
-                error: err
+                error: err,
             });
         }
         connection.query(
-            'INSERT INTO AnnouncementReceiver (announcement_id, receiver_id, has_seen) SELECT ?, User.user_id, 0 FROM User INNER JOIN Department ON Department.department_id = User.department_id INNER JOIN Company ON Department.company_id = Company.company_id WHERE Company.company_id = ?;',
+            "INSERT INTO AnnouncementReceiver (announcement_id, receiver_id, has_seen) SELECT ?, User.user_id, 0 FROM User INNER JOIN Department ON Department.department_id = User.department_id INNER JOIN Company ON Department.company_id = Company.company_id WHERE Company.company_id = ?;",
             [announcementId, companyId],
             (err, results) => {
                 connection.release();
                 if (err) {
                     return res.status(500).send({
-                        error: err
+                        error: err,
                     });
                 }
                 return res.status(200).send({
-                    response: 'AnnouncementReceiver created.', data: {
-                        id: results.insertId, announcementId: announcementId,
-                        body: req.body.body, title: req.body.title, sender: req.body.senderId
-                    }
-                });
+                    response: "AnnouncementReceiver created.",
+                    data: {
+                        id: results.insertId,
+                        announcementId: announcementId,
+                        body: req.body.body,
+                        title: req.body.title,
+                        sender: req.body.senderId,
+                    },//end of data
+                });//end of return
             }
-        );//end of query
-    });//end of mysql.getConnection
-}//end of addAnnouncementReceivers
-
+        ); //end of query
+    }); //end of mysql.getConnection
+} //end of addAnnouncementReceivers
 
 function getAllAnnouncementsForUser(req, res) {
     const userId = req.params.userId;
     mysql.getConnection((err, connection) => {
         if (err) {
             return res.status(500).send({
-                error: err
+                error: err,
             });
         }
         connection.query(
-            'SELECT * FROM Announcement INNER JOIN AnnouncementReceiver where Announcement.announcement_id = AnnouncementReceiver.announcement_id AND receiver_id = ? ORDER BY time ASC;', [userId],
+            "SELECT * FROM Announcement INNER JOIN AnnouncementReceiver where Announcement.announcement_id = AnnouncementReceiver.announcement_id AND receiver_id = ? ORDER BY time ASC;",
+            [userId],
             (err, results) => {
                 connection.release();
                 if (err) {
                     return res.status(500).send({
-                        error: err
+                        error: err,
                     });
                 }
 
-                return res.status(200).send({ response: 'Users found.', data: results });
+                return res
+                    .status(200)
+                    .send({ response: "Users found.", data: results });
             }
-        );//end of query
-    });//end of mysql.getConnection
-}//end of getAllAnnouncementsForUser
+        ); //end of query
+    }); //end of mysql.getConnection
+} //end of getAllAnnouncementsForUser
 
-router.post('/create', adminAuth, createAnnouncement, addAnnouncementReceivers);
-router.get('/get-all-announcements-for-user/:userId', userAuth, getAllAnnouncementsForUser);
+//get first 10 announcements for a user
+function getFirstTenAnnouncementsForUser(req, res) {
+    const userId = req.params.userId;
+    mysql.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).send({
+                error: err,
+            });
+        }
+        connection.query(
+            "SELECT * FROM Announcement INNER JOIN AnnouncementReceiver where Announcement.announcement_id = AnnouncementReceiver.announcement_id AND receiver_id = ? ORDER BY time DESC LIMIT 10;",
+            [userId],
+            (err, results) => {
+                connection.release();
+                if (err) {
+                    return res.status(500).send({
+                        error: err,
+                    });
+                }
+
+                return res
+                    .status(200)
+                    .send({ response: "Announcements found.", data: results });
+            }
+        ); //end of query
+    }); //end of mysql.getConnection
+} //end of getFirstTenAnnouncementsForUser
+
+//get next 10 announcements according to time
+function getNextTenAnnouncementsForUser(req, res) {
+    const userId = req.params.userId;
+    const announcementId = req.body.announcementId;
+    mysql.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).send({
+                error: err,
+            });
+        }
+        connection.query(
+            "SELECT * FROM Announcement INNER JOIN AnnouncementReceiver where Announcement.announcement_id = AnnouncementReceiver.announcement_id AND receiver_id = ? AND time < (SELECT time FROM Announcement WHERE announcement_id = ?) ORDER BY time DESC LIMIT 10;",
+            [userId, announcementId],
+            (err, results) => {
+                connection.release();
+                if (err) {
+                    return res.status(500).send({
+                        error: err,
+                    });
+                }
+                return res.status(200).send({
+                    response: "Announcements found.",
+                    data: results,
+                });
+            }
+        ); //end of query
+    }); //end of mysql.getConnection
+} //end of getNextTenAnnouncementsForUser
+
+
+
+router.post("/create", adminAuth, createAnnouncement, addAnnouncementReceivers);
+router.get(
+    "/get-all-announcements-for-user/:userId",
+    userAuth,
+    getAllAnnouncementsForUser
+);
 
 module.exports = router;
